88cf4859513eb8f8d9c75cb2ac282e302811f1e3
Harry Fischer <harry.j.fischer@gmail.com>
2025-08-06 17:41:34 +0100

    change config

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,11 @@
 <script>
 	import '../lib/styles/main.scss';
 	import guardianLogo from '../lib/assets/guardian-design-logo.svg';
+	import { base } from '$app/paths';
 </script>
 
 <section class="home-page">
 	<img src={guardianLogo} alt="Guardian Design Logo" />
 	<h1>Editorial Design</h1>
-	<a href="/cub-sans">Cub Sans</a>
+	<a href="{base}/cub-sans">Cub Sans</a>
 </section>
