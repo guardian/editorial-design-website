b3195f23c04209099f5d283e2cc54a83afa25666
Harry Fischer <harry.j.fischer@gmail.com>
2025-08-26 11:57:59 +0100

    new

--- svelte.config.js
+++ svelte.config.js
@@ -16,7 +16,7 @@ const config = {
 			strict: false
 		}),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '' : ''
+			base: process.env.NODE_ENV === 'production' ? '/editorial-design-website' : ''
 		},
 		prerender: {
 			handleHttpError: 'warn'
