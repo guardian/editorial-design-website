06d4939366a40058574d76deaf938fbe05c79d7c
Pip Lev <pip.lev@guardian.co.uk>
2026-01-15 14:55:46 +0000

    feat: initial files for snap slides

--- src/routes/interactive-embed/components/Snap-slides.svelte
+++ src/routes/interactive-embed/components/Snap-slides.svelte
@@ -1,131 +1,69 @@
 <script>
-	import { json } from '@sveltejs/kit';
-	import { escapeForSingleQuotedAttr } from '../utils';
-	import { get } from 'svelte/store';
-
-	// This component allows to build configuration containers for interactive atoms.
+	import { buildSnippet, stripUndefined, copy } from '../utils';
 	const { onBack } = $props();
 
-	let atomProps = $state([]);
+	let theme = $state('feature-light');
+  let hasOpacity = $state(false);
+	let bgOpacity = $derived.by(() => hasOpacity ? "40%" : "60%");
 
-	let isPretty = $state(false);
-	let atomName = $state('snap-slides');
-	let atomUrl = $state('');
-	let copiedMsg = $state('');
-	let errorMsg = $state('');
-	let inputValues = $state([]);
-	let jsonAttrs = $state('');
-	let jsonText = $derived.by(() => {
-		try {
-			// Validate JSON
-			JSON.parse(jsonAttrs);
-			const escaped = escapeForSingleQuotedAttr(jsonAttrs);
-			return `<div class="interactive" data-props='${escaped}'></div>`;
-		} catch (e) {
-			return e.message;
-		}
-	});
+	const snapSlidesObj = $derived.by(() =>
+		stripUndefined({
+			element: 'snap-slides',
+			theme: theme || undefined,
+			bgOpacity: bgOpacity
+		})
+	);
 
-	const initInputValues = () => {
-		const arr = [];
-		atomProps.forEach((prop) => {
-			// Initialize with default values
-			if (prop.type === 'checkbox') {
-				arr.push(prop.value || false);
-			} else {
-				arr.push(prop.value || '');
-			}
-		});
-		inputValues = [...arr];
+	let outSnapSlides = $derived(buildSnippet(snapSlidesObj));
+	let msgSnapSlides = $state('');
+
+	const resetSnapSlides = () => {
+		theme = 'feature-light';
+		bgOpacity = '60%';
 	};
-
-	$effect(() => {
-		if (atomName && atomProps) {
-			initInputValues();
-		}
-	});
-
-	const setJsonAttrs = () => {
-		if (atomProps) {
-			const propsSnap = $state.snapshot(atomProps);
-			const obj = {};
-			propsSnap.forEach((prop, index) => {
-				obj[prop.name] = inputValues[index];
-			});
-			jsonAttrs = JSON.stringify(obj, null, isPretty ? 2 : 0);
-		}
-	};
-
-	$effect(() => {
-		if (inputValues && atomProps) {
-			setJsonAttrs();
-		}
-	});
-
-	function resetDev() {
-		inputValues = [];
-		initInputValues();
-		jsonAttrs = '';
-		isPretty = false;
-		errorMsg = '';
-		copiedMsg = '';
-	}
 </script>
 
-<section class="view active" aria-labelledby="dev-heading">
+<section class="view active" aria-labelledby="colour-heading">
 	<div class="panel">
 		<div class="title-row">
-			<h2 id="dev-heading">Snap Slides - Coming soon</h2>
+			<h2 id="snapslides-heading">Snap Slides</h2>
 			<button class="back" onclick={() => onBack()}>← Back</button>
 		</div>
 
-		<!-- <form autocomplete="off">
+		<form autocomplete="off">
 			<div class="row">
-				{#if atomName && atomUrl}
-					<p class="hint">
-						The atom url for <strong>{atomName}</strong> is: <em>{atomUrl}</em>
-					</p>
-				{/if}
-			</div>
+				<div class="field">
+					<label for="theme">Theme</label>
+					<select id="theme" bind:value={theme}>
+						<option value="feature-light">Feature Light</option>
+						<option value="feature-dark">Feature Dark</option>
+						<option value="news">News</option>
+					</select>
+				</div>
 
-			<div class="row">
-				{#if atomProps}
-					{#each atomProps as prop, index}
-						<div class="field">
-							{#if prop.type === 'checkbox'}
-								<label class="switch" for={prop.name}>
-									<input id={prop.name} type="checkbox" bind:checked={inputValues[index]} />
-									<span>{prop.description}</span>
-								</label>
-							{/if}
-						</div>
-					{/each}
-				{/if}
-			</div>
-
-			<div class="field">
-				<label class="switch" for="dev-pretty">
-					<input id="dev-pretty" type="checkbox" bind:checked={isPretty} />
-					<span>Pretty-print JSON (for readability only)</span>
-				</label>
+				<div class="field">
+					<label class="switch" for="bg-opacity">
+						<input id="bg-opacity" type="checkbox" bind:checked={hasOpacity} />
+						<span>Cards have see-through backgrounds</span>
+					</label>
+				</div>
 			</div>
 		</form>
-		<br />
+
 		<div class="out">
 			<div class="actions">
-				<button
-					class="btn"
-					onclick={() => import('../utils').then(m => m.copy(jsonText, (m2)=>copiedMsg=m2))}
+				<button class="btn" onclick={() => copy(outSnapSlides, (m) => (msgSnapSlides = m))}
 					>Copy to clipboard</button
 				>
-				<button class="btn secondary" onclick={resetDev}>Reset</button>
+				<button class="btn secondary" onclick={resetSnapSlides}>Reset</button>
 			</div>
 
-			<textarea class="snippet" readonly bind:value={jsonText}></textarea>
-			<p class="hint" role="status" aria-live="polite">{copiedMsg}</p>
-			{#if errorMsg}
-				<p class="error" aria-live="assertive">{errorMsg}</p>
-			{/if}
-		</div> -->
+			<textarea class="snippet" readonly bind:value={outSnapSlides}></textarea>
+
+			<div class="hint" aria-live="polite">
+				Without any settings the atom will default to the Feature Light theme, without the Explainer
+				tag.
+			</div>
+		</div>
 	</div>
 </section>
